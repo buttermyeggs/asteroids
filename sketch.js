92d5838d55eb0de4da12bef2c9541e2399e95cd0
unknown <delicana.mc@gmail.comgit config --global user.name mae>
2020-01-30 10:35:34 +0100

    added lasers

--- sketch.js
+++ sketch.js
@@ -1,22 +1,34 @@
 let ship;
 let asteroids = [];
+let lasers = [];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	ship = new Ship();
-	asteroids.push(new Asteroids());
+	for (let i = 0; i < 5; i++) {
+	asteroids.push(new Asteroid());
+ }
 }
 
 function draw() {
 	background(0);
+
+	for(let i = 0; i < asteroids.length; i++) {
+		asteroids[i].render();
+		asteroids[i].update();
+		asteroids[i].edges();
+	}
+
+	for(let i = 0; i < lasers.length; i++) {
+		lasers[i].render();
+		lasers[i].update();
+	}
+
 	ship.render();
 	ship.turn();
 	ship.update();
 	ship.edges();
 
-	for(let i = 0; i < asteroids.length; i++) {
-		asteroids[i].render();
-	}
 } 
 
 function keyReleased(){
@@ -25,7 +37,9 @@ function keyReleased(){
 }
 
 function keyPressed(){
-	if (keyCode == RIGHT_ARROW) {
+	if (key == ' '){ 
+		 lasers.push(new Laser(ship.pos, ship.heading));
+	} else if (keyCode == RIGHT_ARROW) {
 		ship.setRotation(0.1);
 	} else if (keyCode == LEFT_ARROW) {
 		ship.setRotation(-0.1);
